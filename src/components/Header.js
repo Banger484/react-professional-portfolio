bd0d29b68f84784ae8ba53d88a04eb3b94c25b9f
Bret Banger <Banger484@gmail.com>
2022-05-22 19:03:53 -0500

    working through layout changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,35 +3,59 @@ import "./Header.css";
 export default function Header() {
   return (
     <header>
-      <div className="header-left">
+      {/* <div className="header-top">
         <img
           src={require("../assets/imgs/my-avatar.jpg")}
           alt="Picture of Bret"
         />
-        <h1 className="header-name">
-          Bret Banger <br />
+        <div>
+          <h1 className="header-name">Bret Banger</h1>
           <h3 className="header-title">Junior Full-Stack Developer</h3>
-        </h1>
+        </div>
       </div>
-      <div className="header-right">
-          <nav className="header-nav">
-            <a href="/">
+      <div className="header-bottom">
+        <nav className="header-nav">
+          <a href="/">
             <button>Home</button>
-            </a>
-            <a href="/about">
+          </a>
+          <a href="/about">
             <button>About</button>
-            </a>
-            <a href="/skills">
+          </a>
+          <a href="/skills">
             <button>Skills</button>
-            </a>
-            <a href="/projects">
+          </a>
+          <a href="/projects">
             <button>Projects</button>
-            </a>
-            <a href="/resume">
+          </a>
+          <a href="/resume">
             <button>Resume</button>
-            </a>
-          </nav>
+          </a>
+        </nav>
+      </div> */}
+      <nav class="navbar bg-navbar navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav links-right">
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About Me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#my-goal">My Goal</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#my-technical-skills">Technical Skills</a>
+          </li>    
+          <li class="nav-item">
+            <a class="nav-link" href="#my-projects">My Projects</a>
+          </li> 
+        </ul>
       </div>
+    </div>
+  </nav>
     </header>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,40 +6,47 @@ export default function Projects () {
             title: 'Weather Dashboard',
             deployed: 'https://banger484.github.io/weather-dashboard/',
             repo: 'https://github.com/Banger484/weather-dashboard',
-            img: 'imgs/projects/Screenshot (50).png'
+            img: 'weather-dashboard'
         },
         {
             title: 'Note Master',
             deployed: 'https://fierce-oasis-24379.herokuapp.com/',
             repo: 'https://github.com/Banger484/note-master',
-            img: 'imgs/projects/Screenshot (41).png'
+            img: 'note-master'
         },
         {
             title: 'Password Generator',
             deployed: 'https://banger484.github.io/password-generator/',
             repo: 'https://github.com/Banger484/password-generator',
-            img: 'imgs/projects/Screenshot (53).png'
+            img: 'password-generator'
         },
         {
             title: 'Cocktail Menu',
             deployed: 'https://banger484.github.io/cocktail-menu/',
             repo: 'https://github.com/Banger484/cocktail-menu',
-            img: 'imgs/projects/Screenshot (54).png'
+            img: 'cocktail-menu'
         }
 
-    ] 
+    ]
+
     return (
         <div className='projects-container'>
             <div className='project-header'>
                 <h1>Projects</h1>
                 <hr />
-                <select className='project-dropdown'>
-                    <option>-- Select a Project you would like to view --</option>
-            {projects.map((project) => <option key={project.title}>{project.title}</option>)}
-                </select>
-
             </div>
             <div className='project-content'>
+            {projects.map((project) => 
+            <div className='project-card' key={project.title}>
+                <h1>{project.title}</h1>
+                <img alt='screenshot of application' src={`images/projects/${project.img}.png`} />
+                <div className='project-card-links-container'>
+                <a href={project.deployed}>Visit Deployed Application</a>
+                <a href={project.repo}>Visit GitHub Repository</a>
+                </div>
+                <hr />
+            </div>
+            )}
             </div>
         </div>
     )
